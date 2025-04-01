480d523c156eed91fd48c1512325b59dd2a4e868
Guilherme Paulino Ribairo <guilhermepaulinoribeiro@gmail.com>
2025-03-31 23:00:48 -0300

    Nomes de variais alterados, outras mudanças menores

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 document.getElementById("loginForm").addEventListener("submit", async function(event) {
     event.preventDefault();
 
-    const email = document.getElementById("email").value;
+    const usuario = document.getElementById("user").value;
     const senha = document.getElementById("senha").value;
 
     try {
         const response = await fetch('users.json');
         const users = await response.json();
 
-        const user = users.find(u => u.email === email && u.senha === senha);
+        const user = users.find(u => u.usuario === usuario && u.senha === senha);
+
 
         if (user) {
             window.location.href = `bem_vindo.html?nome=${encodeURIComponent(user.nome)}`;
         } else {
+         
             alert("Credenciais inválidas!");
+            
         }
     } catch (error) {
         console.error("Erro ao carregar usuários:", error);
